51e6b691af88dc9e51926b44369d6b6dae38559c
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-02-22 23:17:08 +0800

    Done!

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -11,6 +14,11 @@ public class Practice1 {
      *  生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        Random random = new Random();
+        Set<Integer> integers = new HashSet<>();
+        while (integers.size()< 10) {
+            integers.add(random.nextInt(20));
+        }
+        return integers;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,18 +1,39 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        List<Integer> ints = getNumbers();
+        for (int i = 0; i < ints.size(); i++) {
+            System.out.print(ints.get(i) + " ");
+        }
+        System.out.println("==========================");
+
+        for (Integer i : ints) {
+            System.out.print(i + " ");
+        }
+        System.out.println("==========================");
+
+        Iterator<Integer> iterator = ints.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
     public static List<Integer> getNumbers() {
-        return null;
+        ArrayList<Integer> ints = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            ints.add(i);
+        }
+        return ints;
     }
 
 
